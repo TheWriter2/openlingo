2c2ad35ee485117719d76fe6c653a5369f10f9f2
TheWriter <netto9903@gmail.com>
2024-05-22 22:13:57 -0300

    Add initial definitions

---
+++ defs.py
@@ -0,0 +1,17 @@
+class Word:
+    def __init__(self):
+        self.word = ""
+        self.translations = [""]
+        self.word_class = ""
+        self.word_subclass = ""
+        self.radical = ""
+
+class Language:
+    def __init__(self):
+        self.word_list = {pass}
+        self.from_name = ""
+        self.official_name = ""
+
+class Lesson:
+    def __init__(self):
+        
